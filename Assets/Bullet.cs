92829455e1a2ff5628e71a54ec56927f2f8a69f9
VRRobbie <Robbie@Robs-MacBook-Pro.local>
2016-06-06 12:14:36 -0500

    Spencers project commit.

---
+++ Assets/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision){
+		Destroy (gameObject);
+		var hit = collision.gameObject;
+		var health = hit.GetComponent<Health> ();
+		if (health != null) 
+		{
+			int damage = Random.Range(0, 11);
+			if (damage == 0) {
+				damage = 50;			
+			}
+			health.TakeDamage (damage);
+			Debug.Log (damage);
+		}
+	}
+}
