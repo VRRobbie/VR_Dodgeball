8e2bff6b96b8c0311f8369701107ad21f70fe605
VRRobbie <Robbie@Robs-MBP.uno.aces-aoe.com>
2016-06-08 13:54:39 -0500

    Fixed up stadium and fiddled with character look, added "hands" and glasses.

--- Assets/OVR/Scripts/Util/OVRPlayerController.cs
+++ Assets/OVR/Scripts/Util/OVRPlayerController.cs
@@ -229,6 +229,11 @@ public class OVRPlayerController : MonoBehaviour
 
 		bool dpad_move = false;
 
+		if (Input.GetKey(KeyCode.Space))
+		{
+			Jump ();
+		}
+
 		if (OVRInput.Get(OVRInput.Button.DpadUp))
 		{
 			moveForward = true;
@@ -249,8 +254,8 @@ public class OVRPlayerController : MonoBehaviour
 			MoveScale = 0.70710678f;
 
 		// No positional movement if we are in the air
-		if (!Controller.isGrounded)
-			MoveScale = 0.0f;
+		//if (!Controller.isGrounded)
+			//MoveScale = 0.0f;
 
 		MoveScale *= SimulationRate * Time.deltaTime;
 
