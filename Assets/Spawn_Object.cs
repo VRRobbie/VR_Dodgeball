b8456c7b215a3a53588347eee8ce40504d26596b
VRRobbie <Robbie@Robs-MacBook-Pro.local>
2016-06-10 12:45:23 -0500

    Added spawning script for dodgeballs and player.

---
+++ Assets/Spawn_Object.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn_Object : MonoBehaviour {
+
+
+	public Transform spawnpoint;
+	public GameObject spawnObject;
+
+	void Start () 
+	{
+		Spawn ();
+	}
+	
+	void Spawn ()
+	{
+		Instantiate (spawnObject, spawnpoint.position, spawnpoint.rotation);
+	}
+}
