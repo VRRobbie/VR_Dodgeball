990b0ce83d133719843628f5f57ff232deb7848f
VRRobbie <Robbie@Robs-MacBook-Pro.local>
2016-06-14 11:58:21 -0500

    Added VR Interaction, Highlight.

---
+++ Assets/Scripts/PickUpBall.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine.VR;
+using VRStandardAssets.Utils;
+
+public class PickUpBall : MonoBehaviour {
+
+	[SerializeField] private VRInteractiveItem m_InteractiveItem;
+	[SerializeField] private Shader defaultShader;
+	[SerializeField] private Shader hoverShader;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//hoverShader = Shader.Find ("Highlight");
+	
+	}
+
+	private void OnEnable()
+	{
+		m_InteractiveItem.OnOver += HandleOver;
+		m_InteractiveItem.OnOut += HandleOut;
+		m_InteractiveItem.OnClick += HandleClick;
+	}
+
+	private void OnDisable()
+	{
+		m_InteractiveItem.OnOver -= HandleOver;
+		m_InteractiveItem.OnOut -= HandleOut;
+		m_InteractiveItem.OnClick -= HandleClick;
+	}
+
+	private void HandleOver()
+	{
+		Debug.Log ("Looking");
+		HighlightObject ();
+	}
+
+	private void HandleOut()
+	{
+		Debug.Log ("Not looking");
+		DeHighlightObject ();
+	}
+
+	private void HandleClick()
+	{
+		Debug.Log ("Look and click brotha!");
+	}
+
+	private void HighlightObject()
+	{
+		m_InteractiveItem.GetComponent<Renderer> ().material.shader = hoverShader;
+	}
+
+	private void DeHighlightObject()
+	{
+		m_InteractiveItem.GetComponent<Renderer> ().material.shader = defaultShader;
+	}
+}
---
+++ Assets/Scripts/ShaderChange.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+
+public class ShaderChange : NetworkBehaviour {
+
+
+	public Shader highlight;
+	public GameObject bullet;
+
+	// Update is called once per frame
+	void Update () {
+		highlight = Shader.Find("Highlight");
+		bullet.GetComponent<Renderer> ().material.shader = highlight;
+	}
+}
