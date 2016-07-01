41442d3f7741541230003993dc4458fcfb57fcb3
VRRobbie <rmorrissey@unomaha.edu>
2016-07-01 12:38:33 -0600

    Commented out maxs main camera code. Added playersetup changes script to player.

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -5,24 +5,25 @@ using UnityEngine.Networking;
 public class PlayerSetup : NetworkBehaviour {
     [SerializeField]
     public Behaviour[] componentsToDisable;
-    Camera sceneCamera;
+    //Camera sceneCamera;
 	// Use this for initialization
 	void Start () {
         if (!isLocalPlayer) {
             for(int i = 0; i < componentsToDisable.Length; i++) {
                 componentsToDisable[i].enabled = false;
             }
-        }else {
-            sceneCamera = Camera.main;
-            print("Camera turning off: " + sceneCamera.name);
-            if(sceneCamera != null)
-               sceneCamera.gameObject.SetActive(false);
         }
+//		else {
+//            sceneCamera = Camera.main;
+//            print("Camera turning off: " + sceneCamera.name);
+//            if(sceneCamera != null)
+//               sceneCamera.gameObject.SetActive(false);
+//        }
 	}
 
-    void Update() {
-        if (sceneCamera != null && !sceneCamera.gameObject.activeSelf){
-            sceneCamera.gameObject.SetActive(true);
-        }
-    }
+//    void Update() {
+//        if (sceneCamera != null && !sceneCamera.gameObject.activeSelf){
+//            sceneCamera.gameObject.SetActive(true);
+//        }
+//    }
 }
